1083580d5557f8e8101e947b2a97bb0a67e22a34
billy.berkouwer <billy.berkouwer@sharpend.com>
2023-01-19 15:20:05 +0000

    custom-toon-shader

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,122 +2,418 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { useEffect, useState, useRef, useCallback } from 'react'
+import { ShaderMaterial, PointLight, AmbientLight, Mesh } from 'three'
+import { MutableRefObject } from 'react'
+import { UniformsUtils } from 'three/src/renderers/shaders/UniformsUtils.js'
 
-const inter = Inter({ subsets: ['latin'] })
+import {
+	Color,
+	Vector3
+} from 'three';
+
+/**
+ * Currently contains:
+ *
+ *	toon1
+ *	toon2
+ *	hatching
+ *	dotted
+ */
+
+const ToonShader1 = {
+
+	uniforms: {
+
+		'uDirLightPos': { value: new Vector3() },
+		'uDirLightColor': { value: new Color( 0xeeeeee ) },
+
+		'uAmbientLightColor': { value: new Color( 0x050505 ) },
+
+		'uBaseColor': { value: new Color( 0xffffff ) }
+
+	},
+
+	vertexShader: /* glsl */`
+
+		varying vec3 vNormal;
+		varying vec3 vRefract;
+
+		void main() {
+
+			vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
+			vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
+			vec3 worldNormal = normalize ( mat3( modelMatrix[0].xyz, modelMatrix[1].xyz, modelMatrix[2].xyz ) * normal );
+
+			vNormal = normalize( normalMatrix * normal );
+
+			vec3 I = worldPosition.xyz - cameraPosition;
+			vRefract = refract( normalize( I ), worldNormal, 1.02 );
+
+			gl_Position = projectionMatrix * mvPosition;
+
+		}`,
+
+	fragmentShader: /* glsl */`
+
+		uniform vec3 uBaseColor;
+
+		uniform vec3 uDirLightPos;
+		uniform vec3 uDirLightColor;
+
+		uniform vec3 uAmbientLightColor;
+
+		varying vec3 vNormal;
+
+		varying vec3 vRefract;
+
+		void main() {
+
+			float directionalLightWeighting = max( dot( normalize( vNormal ), uDirLightPos ), 0.0);
+			vec3 lightWeighting = uAmbientLightColor + uDirLightColor * directionalLightWeighting;
+
+			float intensity = smoothstep( - 0.5, 1.0, pow( length(lightWeighting), 20.0 ) );
+			intensity += length(lightWeighting) * 0.2;
+
+			float cameraWeighting = dot( normalize( vNormal ), vRefract );
+			intensity += pow( 1.0 - length( cameraWeighting ), 6.0 );
+			intensity = intensity * 0.2 + 0.3;
+
+			if ( intensity < 0.50 ) {
+
+				gl_FragColor = vec4( 2.0 * intensity * uBaseColor, 1.0 );
+
+			} else {
+
+				gl_FragColor = vec4( 1.0 - 2.0 * ( 1.0 - intensity ) * ( 1.0 - uBaseColor ), 1.0 );
+
+		}
+
+		}`
+
+};
+
+const ToonShader2 = {
+
+	uniforms: {
+
+		'uDirLightPos': { value: new Vector3() },
+		'uDirLightColor': { value: new Color( 0xeeeeee ) },
+
+		'uAmbientLightColor': { value: new Color( 0x050505 ) },
+
+		'uBaseColor': { value: new Color( 0xeeeeee ) },
+		'uLineColor1': { value: new Color( 0x808080 ) },
+		'uLineColor2': { value: new Color( 0x000000 ) },
+		'uLineColor3': { value: new Color( 0x000000 ) },
+		'uLineColor4': { value: new Color( 0x000000 ) }
+
+	},
+
+	vertexShader: /* glsl */`
+
+		varying vec3 vNormal;
+
+		void main() {
+
+			gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+			vNormal = normalize( normalMatrix * normal );
+
+		}`,
+
+	fragmentShader: /* glsl */`
+
+		uniform vec3 uBaseColor;
+		uniform vec3 uLineColor1;
+		uniform vec3 uLineColor2;
+		uniform vec3 uLineColor3;
+		uniform vec3 uLineColor4;
+
+		uniform vec3 uDirLightPos;
+		uniform vec3 uDirLightColor;
+
+		uniform vec3 uAmbientLightColor;
+
+		varying vec3 vNormal;
+
+		void main() {
+
+			float camera = max( dot( normalize( vNormal ), vec3( 0.0, 0.0, 1.0 ) ), 0.4);
+			float light = max( dot( normalize( vNormal ), uDirLightPos ), 0.0);
+
+			gl_FragColor = vec4( uBaseColor, 1.0 );
+
+			if ( length(uAmbientLightColor + uDirLightColor * light) < 1.00 ) {
+
+				gl_FragColor *= vec4( uLineColor1, 1.0 );
+
+			}
+
+			if ( length(uAmbientLightColor + uDirLightColor * camera) < 0.50 ) {
+
+				gl_FragColor *= vec4( uLineColor2, 1.0 );
+
+			}
+
+		}`
+
+};
+
+const ToonShaderHatching = {
+
+	uniforms: {
+
+		'uDirLightPos':	{ value: new Vector3(2,2,2) },
+		'uDirLightColor': { value: new Color( 0xffffff ) },
+
+		'uAmbientLightColor': { value: new Color( 0x050505 ) },
+
+		'uBaseColor': { value: new Color( 0xFFA500 ) },
+		'uLineColor1': { value: new Color( 0xFF0000 ) },
+		'uLineColor2': { value: new Color( 0xFF0000 ) },
+		'uLineColor3': { value: new Color( 0xFF0000 ) },
+		'uLineColor4': { value: new Color( 0xF00000 ) },
+		'uLineColor5': { value: new Color( 0xFBCEB1 ) },
+		'uLineColor6': { value: new Color( 0xFBCEB1 ) },
+
+	},
+
+	vertexShader: /* glsl */`
+
+		varying vec3 vNormal;
+
+		void main() {
+
+			gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+			vNormal = normalize( normalMatrix * normal );
+
+		}`,
+
+	fragmentShader: /* glsl */`
+
+		uniform vec3 uBaseColor;
+		uniform vec3 uLineColor1;
+		uniform vec3 uLineColor2;
+		uniform vec3 uLineColor3;
+		uniform vec3 uLineColor4;
+		uniform vec3 uLineColor5;
+		uniform vec3 uLineColor6;
+
+		uniform vec3 uDirLightPos;
+		uniform vec3 uDirLightColor;
+
+		uniform vec3 uAmbientLightColor;
+
+		varying vec3 vNormal;
+
+		void main() {
+
+			float directionalLightWeighting = max( dot( normalize(vNormal), uDirLightPos ), 0.0);
+			vec3 lightWeighting = uAmbientLightColor + uDirLightColor * directionalLightWeighting;
+
+			gl_FragColor = vec4( uBaseColor, 1.0 );
+
+      if ( length(lightWeighting) > 4.999 ) {
+
+				if (mod(gl_FragCoord.x + gl_FragCoord.y - 5.0, 5.0) == 0.0) {
+
+					gl_FragColor = vec4( uLineColor6, 1.0 );
+
+				}
+
+			}
+      
+      if ( length(lightWeighting) > 4.999 ) {
+
+				if (mod(gl_FragCoord.x - gl_FragCoord.y, 5.0) == 0.0) {
+
+					gl_FragColor = vec4( uLineColor6, 1.0 );
+
+				}
+
+			}
+
+      if ( length(lightWeighting) > 3.0 ) {
+
+				if (mod(gl_FragCoord.x - gl_FragCoord.y, 10.0) == 0.0) {
+
+					gl_FragColor = vec4( uLineColor6, 1.0 );
+
+				}
+
+			}
+
+      if ( length(lightWeighting) > 2.0 ) {
+
+				if ( mod(gl_FragCoord.x + gl_FragCoord.y + 5.0, 10.0) == 0.0) {
+
+					gl_FragColor = vec4( uLineColor5, 1.0 );
+
+				}
+
+			}
+
+			if ( length(lightWeighting) < 1.00 ) {
+
+				if ( mod(gl_FragCoord.x + gl_FragCoord.y, 5.0) == 0.0) {
+
+					gl_FragColor = vec4( uLineColor1, 1.0 );
+
+				}
+
+			}
+
+			if ( length(lightWeighting) < 0.75 ) {
+
+				if (mod(gl_FragCoord.x - gl_FragCoord.y, 10.0) == 0.0) {
+
+					gl_FragColor = vec4( uLineColor2, 1.0 );
+
+				}
+
+			}
+
+			if ( length(lightWeighting) < 0.50 ) {
+
+				if (mod(gl_FragCoord.x + gl_FragCoord.y - 5.0, 10.0) == 0.0) {
+
+					gl_FragColor = vec4( uLineColor3, 1.0 );
+
+				}
+
+			}
+
+			if ( length(lightWeighting) < 0.3465 ) {
+
+				if (mod(gl_FragCoord.x - gl_FragCoord.y - 5.0, 10.0) == 0.0) {
+
+					gl_FragColor = vec4( uLineColor4, 1.0 );
+
+			}
+
+			}
+
+		}`
+
+};
+
+const ToonShaderDotted = {
+
+	uniforms: {
+
+		'uDirLightPos':	{ value: new Vector3() },
+		'uDirLightColor': { value: new Color( 0xeeeeee ) },
+
+		'uAmbientLightColor': { value: new Color( 0x050505 ) },
+
+		'uBaseColor': { value: new Color( 0xffffff ) },
+		'uLineColor1': { value: new Color( 0x000000 ) }
+
+	},
+
+	vertexShader: /* glsl */`
+
+		varying vec3 vNormal;
+
+		void main() {
+
+			gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+			vNormal = normalize( normalMatrix * normal );
+
+		}`,
+
+	fragmentShader: /* glsl */`
+
+		uniform vec3 uBaseColor;
+		uniform vec3 uLineColor1;
+		uniform vec3 uLineColor2;
+		uniform vec3 uLineColor3;
+		uniform vec3 uLineColor4;
+
+		uniform vec3 uDirLightPos;
+		uniform vec3 uDirLightColor;
+
+		uniform vec3 uAmbientLightColor;
+
+		varying vec3 vNormal;
+
+		void main() {
+
+		float directionalLightWeighting = max( dot( normalize(vNormal), uDirLightPos ), 0.0);
+		vec3 lightWeighting = uAmbientLightColor + uDirLightColor * directionalLightWeighting;
+
+		gl_FragColor = vec4( uBaseColor, 1.0 );
+
+		if ( length(lightWeighting) < 1.00 ) {
+
+				if ( ( mod(gl_FragCoord.x, 4.001) + mod(gl_FragCoord.y, 4.0) ) > 6.00 ) {
+
+					gl_FragColor = vec4( uLineColor1, 1.0 );
+
+				}
+
+			}
+
+			if ( length(lightWeighting) < 0.50 ) {
+
+				if ( ( mod(gl_FragCoord.x + 2.0, 4.001) + mod(gl_FragCoord.y + 2.0, 4.0) ) > 6.00 ) {
+
+					gl_FragColor = vec4( uLineColor1, 1.0 );
+
+				}
+
+			}
+
+		}`
+
+};
+
+const inter = Inter({ subsets: ['latin'] });
+
+function createShaderMaterial( shader: any, light: any, ambientLight: any ) {
+  var u = UniformsUtils.clone( shader.uniforms );
+  var vs = shader.vertexShader;
+  var fs = shader.fragmentShader;
+  var material = new ShaderMaterial( { uniforms: u, vertexShader: vs, fragmentShader: fs } );
+  material.uniforms[ "uDirLightPos" ].value = light.position;
+  material.uniforms[ "uDirLightColor" ].value = light.color;
+  material.uniforms[ "uAmbientLightColor" ].value = ambientLight.color;
+  return material;
+}
 
 export default function Home() {
+  const [toonMaterial, setToonMaterial] = useState({
+    uniforms: {},
+    fragmentShader: '',
+    vertexShader: '',
+  })
+
+  useEffect(() => {
+    setToonMaterial(ToonShaderHatching)
+  
+    return () => {
+      
+    }
+  }, [])
+  
+  const pointLight = useRef() as MutableRefObject<PointLight | null>;
+  const ambientLight = useRef() as MutableRefObject<AmbientLight | null>
+  const mesh = useRef() as MutableRefObject<Mesh | null>
+
   return (
-    <>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-    </>
+    <div className={styles.main}>
+      <Canvas>
+        <pointLight ref={el => pointLight.current = el} position={[2, 2, 0]} intensity={0.5}/>
+        <ambientLight ref={el => ambientLight.current = el} intensity={0.1}/>
+        <mesh ref={el => mesh.current = el}>
+          {/* <cylinderGeometry /> */}
+          <sphereGeometry args={[1,32,16]} />
+          <shaderMaterial uniforms={toonMaterial.uniforms} fragmentShader={toonMaterial.fragmentShader} vertexShader={toonMaterial.vertexShader} />
+        </mesh>
+        <OrbitControls />
+      </Canvas>
+    </div>
   )
 }
